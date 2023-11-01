08982f4ae9f47ffc59d559c2263a3dde6a2569f7
temlena <oridupahelen@gmail.com>
2023-11-01 16:19:46 +0100

    added functionality

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-
 const adviceBtn = document.querySelector('.advice-generator__btn-img');
 const adviceNumber = document.querySelector('.advice-container__advice-number')
 const adviceQuoteText = document.querySelector('.advice-container__advice-quote')
 
 const fetchRandomAdvice = async()=>{
-    const response = await fetch ("https://api.adviceslip.com")
-    console.log(response)
-    const advice =  await response.json()
-    console.log(advice)
-    // return data
+    // fetch data from API
+    const response = await fetch ("https://api.adviceslip.com/advice")
+    const data = await response.json()
+    console.log(data)
+    const advice = data.slip
+    adviceNumber.textContent = `ADVICE #${advice.id}`
+    adviceQuoteText.textContent = `"${advice.advice}"`
 }
-fetchRandomAdvice()
 
-// adviceBtn.addEventListener("click",);
+// const generateAdvice= async()=>{
+//     const quotes = await fetchRandomAdvice()
+//     const adviceWord = quotes.slip
+//     console.log(adviceWord)
+//     adviceNumber.textContent = `ADVICE ${adviceWord.id}`
+//     adviceQuoteText.textContent = `"${adviceWord.advice}"`
+// }
+
+// adviceBtn.addEventListener("click", generateAdvice);
+adviceBtn.addEventListener("click",fetchRandomAdvice);
